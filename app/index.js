fe612c5f76c87b3cbab1546d918a51e93a4d695b
Jed Watson <jed.watson@me.com>
2015-08-18 21:04:47 +1000

    Switching to object-assign and speaking url for compatibility with latest Yeoman

--- app/index.js
+++ app/index.js
@@ -1,16 +1,19 @@
 'use strict';
-var chalk = require('chalk'),
-	yeoman = require('yeoman-generator');
+
+var assign = require('object-assign');
+var chalk = require('chalk');
+var speakingurl = require('speakingurl');
+var yeoman = require('yeoman-generator');
 
 var ReactExpressGenerator = yeoman.generators.Base.extend({
-	
+
 	initializing: function() {
 		this.pkg = require('../package.json');
 	},
-	
+
 	prompting: function() {
 		var done = this.async();
-		
+
 		this.log(
 			'\n' + chalk.bold.underline('Welcome to the React + Express project generator') +
 			'\n' +
@@ -18,7 +21,7 @@ var ReactExpressGenerator = yeoman.generators.Base.extend({
 			'\n' + chalk.bold('express') + ' for the server, and include ' + chalk.bold('react-router') + ' and ' + chalk.bold('Bootstrap') + '.' +
 			'\n'
 		);
-		
+
 		var prompts = [{
 			type: 'input',
 			name: 'projectName',
@@ -30,18 +33,18 @@ var ReactExpressGenerator = yeoman.generators.Base.extend({
 			message: 'Would you like to create a new directory for your project?',
 			default: true
 		}];
-		
+
 		this.prompt(prompts, function (props) {
 			this.log('\n');
-			this._.extend(this, props);
-			this.projectKey = this._.slugify(this.projectName).toLowerCase();
+			assign(this, props);
+			this.projectKey = speakingurl(this.projectName);
 			if (props.createDirectory) {
 				this.destinationRoot(this.projectKey);
 			}
 			done();
 		}.bind(this));
 	},
-	
+
 	writing: {
 		project: function () {
 			this.copy('server.js', 'server.js');
@@ -49,26 +52,26 @@ var ReactExpressGenerator = yeoman.generators.Base.extend({
 			this.copy('gitignore', '.gitignore');
 			this.template('_package.json', 'package.json');
 		},
-		
+
 		clientfiles: function() {
 			this.copy('client/config.js', 'client/config.js');
 			this.template('client/scripts/_index.js', 'client/scripts/index.js');
 			this.directory('client/routes', 'client/routes');
 		},
-		
+
 		serverfiles: function() {
 			this.template('server/templates/views/_index.html', 'server/templates/views/index.html');
 		},
-		
+
 		publicfiles: function() {
 			this.directory('public', 'public');
 		}
 	},
-	
+
 	install: function () {
 		this.npmInstall();
 	},
-	
+
 	end: function() {
 		var chdir = this.createDirectory ? '"cd ' + this.projectKey + '" then ' : '';
 		this.log(
@@ -82,7 +85,7 @@ var ReactExpressGenerator = yeoman.generators.Base.extend({
 			'\n'
 		);
 	}
-	
+
 });
 
 module.exports = ReactExpressGenerator;
